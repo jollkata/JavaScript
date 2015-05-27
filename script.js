32283f5511b03398bc7bb637a56202069623eaca
jollkata <i.v.bogeva@gmail.com>
2015-05-27 15:31:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+//Problem 1
+var a = 123,
+	b = 123.45,
+	c = true,
+	d = 'hello',
+	e = [1, 2, 'some_text'],
+	f = {some: 'text', text: 'some'};
+
+//Problem 2
+var quoted_text = '\'How you doin\'?\', Joey said\'.'
+console.log(quoted_text);
+
+//Problem 3
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+
+//Problem 4
+var g,
+	h = null;
+console.log(typeof(g));
+console.log(typeof(h));
